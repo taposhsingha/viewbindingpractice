af4ac788bb1a30da1bfbb932b8a2fc93136353c1
taposhsingha <57071916+taposhsingha@users.noreply.github.com>
2023-02-12 09:23:42 +0600

    example of basic viewbinding code added

---
+++ app/src/main/java/com/example/viewbindingpractice/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.viewbindingpractice
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import com.example.viewbindingpractice.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        //val messageView = findViewById<TextView>(R.id.tvMessage)
+        //val inputText = findViewById<EditText>(R.id.etName)
+        //val submitButton = findViewById<Button>(R.id.btnSubmit)
+        binding.apply {
+            btnSubmit.setOnClickListener {
+                val name = etName.text.toString()
+                tvMessage.text = "Hello! $name"
+                etName.setText("")
+            }
+        }
+    }
+}
